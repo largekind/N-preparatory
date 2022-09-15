ed648f72c8c9c9329abcb05d9dbe6b9c8843a4f9
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-09-15 22:30:27 +0900

    Add calc.js

---
+++ Introduction/javascript/calc.js
@@ -0,0 +1,13 @@
+let tax = 10; //税(%)
+let stationary = 290; //文房具代金
+
+document.write("文房具代 :");
+document.write(stationary * (1.0+(tax/100)));
+document.write('<br>');
+
+let radius = 12;
+
+document.write("半径 :");
+document.write(radius * radius * 3.14);
+document.write('<br>');
+
