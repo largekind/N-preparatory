00dd5f148f9e0e54cf2cd66476bd27f25874d80f
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-09-25 20:34:30 +0900

    Fix double cmd method

--- Preparation/linux-study/bot/niconico-ranking.sh
+++ Preparation/linux-study/bot/niconico-ranking.sh
@@ -8,8 +8,3 @@ mkdir -p $dirname
 filename="${dirname}/hourly-ranking-`date +'%Y%m%d%H%M'`.xml"
 echo "保存しました: $filename"
 curl -s -o $filename -H "User-Agent: CrawlBot; email@example.com" https://www.nicovideo.jp/ranking/genre/all?rss=2.0&lang=ja-jp
-
-filename="${dirname}/hourly-ranking-`date +'%Y%m%d%H%M'`.xml"
-echo "保存しました: $filename"
-
-curl -s -o $filename -H "User-Agent: CrawlBot; email@example.com" https://www.nicovideo.jp/ranking/genre/all?rss=2.0&lang=ja-jp
