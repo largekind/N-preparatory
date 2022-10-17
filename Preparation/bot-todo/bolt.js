502a6cbb65c30a33853f1372be94182ca31b0735
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-10-17 20:31:14 +0900

    Add CRUD method

--- Preparation/bot-todo/bolt.js
+++ Preparation/bot-todo/bolt.js
@@ -1,3 +1,11 @@
+// Description:
+//   TODO を管理できるボットです
+// Commands:
+//   ボット名 add      - TODO を作成
+//   ボット名 done     - TODO を完了にする
+//   ボット名 del      - TODO を消す
+//   ボット名 list     - TODO の一覧表示
+//   ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
 const bolt = require('@slack/bolt');
 const dotenv = require('dotenv');
@@ -11,7 +19,32 @@ const app = new bolt.App({
   logLevel: 'debug'
 });
 
-console.log("現在のTODO:");
-console.log(todo.list());
+//TODO操作の処理
+app.message(/add (.+)/i, ({context, say}) => { //最後のi : 小文字大文字どちらでもok
+  //(.+)にマッチする情報をtaskNameに入れて、todoに追加
+  const taskName = context.matches[1].trim(); //trim()で不要な空白削除
+  todo.add(taskName);
+  say(`追加しました: ${taskName}`);
+});
+
+app.message(/done (.+)/i, ({context, say}) => {
+  const taskName = context.matches[1].trim();
+  todo.done(taskName);
+  say(`完了にしました: ${taskName}`);
+});
+
+app.message(/del (.+)/i, ({context, say}) => {
+  const taskName = context.matches[1].trim();
+  todo.del(taskName);
+  say(`削除しました: ${taskName}`);
+});
+
+app.message(/^list/i, ({context, say}) => {
+  say(todo.list().join('\n'));
+});
+
+app.message(/donelist/i, ({context, say}) => {
+  say(todo.donelist().join('\n'));
+});
 
 app.start();
